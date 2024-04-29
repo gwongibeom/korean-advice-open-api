1a7599136962c49bd48a73fcfac90905de8ee326
gwongibeom <k201gun@kakao.com>
2024-04-29 17:24:18 +0900

    Revert "vercel Analytics 추가"
    This reverts commit 19d7461d13cf8828a3dafec63d37101f2f609c6d.

--- api/advice.ts
+++ api/advice.ts
@@ -1,9 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import _advices from './_advices'
-import { inject } from '@vercel/analytics'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  inject()
   const randomAdvice: object =
     _advices[Math.floor(Math.random() * _advices.length)]
   return res.json({
