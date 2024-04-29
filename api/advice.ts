9ceac68546aa8dc6939f17ade7ef331e7089d47e
gwongibeom <k201gun@kakao.com>
2024-04-29 16:44:36 +0900

    (개발) 랜덤 명언 API 개발

--- api/advice.ts
+++ api/advice.ts
@@ -1,7 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import _advices from './_advices'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  const randomAdvice: object =
+    _advices[Math.floor(Math.random() * _advices.length)]
+  console.log(randomAdvice)
   return res.json({
-    message: `명언!`,
+    ...randomAdvice,
   })
 }
