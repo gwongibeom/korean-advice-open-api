c60f77f156f4047317387b461995f4633a5006df
Gi Beom Gwon <k201gun@kakao.com>
2024-05-02 19:44:39 +0900

    (개발) CORS 이슈 대응

--- api/advice.ts
+++ api/advice.ts
@@ -2,9 +2,17 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import _advices from './_advices'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+`
   const randomAdvice: object =
     _advices[Math.floor(Math.random() * _advices.length)]
-  return res.json({
+
+  res.status(200).json({
     ...randomAdvice,
   })
-}
+}`
